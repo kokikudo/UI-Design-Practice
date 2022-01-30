3c1e3f31e1bcb92f1792f29ccf12e94ba0ddf933
kokikudo <massanmesu@icloud.com>
2022-01-30 11:05:56 +0900

    Initial Commit

---
+++ I am Rich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I am Rich
+//
+//  Created by kudo koki on 2022/01/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ I am Rich/I_am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_RichApp.swift
+//  I am Rich
+//
+//  Created by kudo koki on 2022/01/30.
+//
+
+import SwiftUI
+
+@main
+struct I_am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
